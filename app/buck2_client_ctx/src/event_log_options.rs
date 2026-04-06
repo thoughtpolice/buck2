c265e63cea845971a2f1c0843c986564c05d6a0e
Austin Seipp <aseipp@pobox.com>
2026-04-06 18:35:17 -0500

    log: allow specifying a command to download traces
    The OSS build currently uses 'curl' to download files from a specific
    URL, but a more powerful method is to allow any arbitrary command. I'm exploring
    using the REAPI cache server as a backend for Buck logs, so I'd like to be able
    to do this:
    
        redownload buck2-logs://$LOG_UUID_GOES_HERE /full/path/to/buck-out/v2/logs/...
    
    or something vaguely like that to download from a remote server
    (assuming the Remote Asset API above).
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- app/buck2_client_ctx/src/event_log_options.rs
+++ app/buck2_client_ctx/src/event_log_options.rs
@@ -15,6 +15,7 @@ use buck2_common::temp_path::TempPath;
 use buck2_error::BuckErrorContext;
 use buck2_error::BuckErrorOptionContext;
 use buck2_error::ErrorTag;
+use buck2_error::internal_error;
 use buck2_event_log::file_names::find_log_by_trace_id;
 use buck2_event_log::file_names::retrieve_nth_recent_log;
 use buck2_event_log::read::EventLogPathBuf;
@@ -167,6 +168,26 @@ impl EventLogOptions {
                     "Curl",
                     async_background_command("curl")
                         .args(args)
+                        .stdin(Stdio::null())
+                        .stdout(Stdio::null())
+                        .stderr(Stdio::piped())
+                        .spawn()?,
+                )
+            }
+            LogDownloadMethod::Command(cmd) => {
+                let out_path = temp_path
+                    .path()
+                    .as_os_str()
+                    .to_str()
+                    .ok_or_else(|| internal_error!("temp_path is not valid UTF-8"))?;
+                let expanded = cmd
+                    .replace("$TRACE_ID", &trace_id.to_string())
+                    .replace("$OUT", out_path);
+                crate::eprintln!("Spawning: sh -c {}", expanded)?;
+                (
+                    "Command",
+                    async_background_command("sh")
+                        .args(["-c", &expanded])
                         .stdin(Stdio::null())
                         .stdout(Stdio::null())
                         .stderr(Stdio::piped())
