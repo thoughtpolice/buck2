26178b5d50bdd83a5af4d467fc7124b5dd347d23
Patrick Riggs <riggspc@meta.com>
2026-04-28 03:25:37 -0700

    Make the OSS checker happy by adding copyright headers
    Summary:
    Got AI to try to resolve some of the copyright header issues listed here
    
    there's still more, these are just the ones that seemed easiest and leas controversial to AI away
    
    Reviewed By: IanChilds
    
    Differential Revision: D102728754
    
    fbshipit-source-id: 8bfd258778af795e2c639708360867657b456ed2

--- games/bin/sokoban_pipeline.py
+++ games/bin/sokoban_pipeline.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is dual-licensed under either the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree or the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree. You may select, at your option, one of the
+# above-listed licenses.
 
 """Sokoban level generation pipeline.
 
